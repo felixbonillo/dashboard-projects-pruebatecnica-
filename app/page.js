6e24fced2905c3101fbfa3cbd35a2d997c53f43b
Felix Bonillo <felixbdev@gmail.com>
2025-08-17 15:32:52 -0400

    Creacion del primer grafico

--- app/page.js
+++ app/page.js
@@ -41,7 +41,7 @@ export default function Home() {
           searchTerm={searchTerm}
           onSearchChange={handleSearchChange}
           statusFilter={statusFilter}
-          onStatusFilterChange={setStatusFilter}
+          onStatusFilterChange={handleStatusFilterChanged}
         />
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-4">
